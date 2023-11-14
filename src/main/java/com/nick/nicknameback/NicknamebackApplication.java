8e61a611fdd5da645bdcee1d294c324b27eefab9
whwjdan <whwjdan@naver.com>
2023-11-14 15:18:19 +0900

    Initial commit

---
+++ src/main/java/com/nick/nicknameback/NicknamebackApplication.java
@@ -0,0 +1,13 @@
+package com.nick.nicknameback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicknamebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicknamebackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nick/nicknameback/NicknamebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.nicknameback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicknamebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
